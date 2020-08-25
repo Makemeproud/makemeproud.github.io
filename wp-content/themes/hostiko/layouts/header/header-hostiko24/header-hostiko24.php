ce81f65ea5edb8460a9e4fd4743c76362a9c5db3
Makemeproud <fossilfly@protonmail.ch>
2020-08-25 05:44:42 -0700

    Missing files initial increment

---
+++ wp-content/themes/hostiko/layouts/header/header-hostiko24/header-hostiko24.php
@@ -0,0 +1,8 @@
+<?php
+/** * Created by PhpStorm. * User: akd * Date: 8/12/2017 * Time: 5:05 PM */
+
+$hostiko_redux_option = get_option( 'opt_theme_options' ); ?>
+
+<?php do_action('bbhb_header') ?>
+
+<div class="clearfix"></div>
